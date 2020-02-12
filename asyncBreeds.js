584263546e014ffc401e285541fc362a759645c1
Zhao Wang <wangz2266@gmail.com>
2020-02-11 20:12:22 -0800

    cb returns undefined when error

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -4,6 +4,8 @@ const breedDetailsFromFile = function(breed, cb) {
   fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
     if (!error) {
       cb(data);
+    } else {
+      cb();
     }
   });
 };
